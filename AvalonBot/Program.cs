eda6f693c1d1a60c75480592adccc6ad7834912c
= <a.flaming.snowman@gmail.com>
2020-06-25 18:32:56 -0700

    added main program

---
+++ AvalonBot/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.Addons.Interactive;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
+
+namespace AvalonBot
+{
+    class Program
+    {
+        static void Main(string[] args) => new Program().RunBotAsync().GetAwaiter().GetResult();
+        private DiscordSocketClient _client;
+        private CommandService _commands;
+        private IServiceProvider _services;
+
+        public async Task RunBotAsync()
+        {
+            _client = new DiscordSocketClient();
+            _commands = new CommandService();
+
+            _services = new ServiceCollection()
+                .AddSingleton(_client)
+                .AddSingleton(_commands)
+                .AddSingleton<InteractiveService>()
+                .AddSingleton<ReliabilityService>()
+                .BuildServiceProvider();
+           
+            //event subscriptions
+            _client.Log += Log;
+
+            await RegisterCommandsAsync();
+
+            await _client.LoginAsync(TokenType.Bot, File.ReadAllText("token.txt"));
+
+            await _client.StartAsync();
+
+            await _client.SetGameAsync("avalon");
+
+            Console.WriteLine("V1");
+
+            await Task.Delay(-1);
+        }
+
+
+        private Task Log(LogMessage arg)
+        {
+            Console.WriteLine(arg);
+
+            return Task.CompletedTask;
+        }
+
+        public async Task RegisterCommandsAsync()
+        {
+            _client.MessageReceived += HandleCommandAsync;
+
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+        }
+
+        private async Task HandleCommandAsync(SocketMessage arg)
+        {
+            var message = arg as SocketUserMessage;
+
+            if (message is null || message.Author.IsBot || message.Author.IsWebhook) return;
+
+            int argPos = 0;
+
+            if (message.HasStringPrefix("!", ref argPos) || message.HasMentionPrefix(_client.CurrentUser, ref argPos))
+            {
+                var context = new SocketCommandContext(_client, message);
+
+                var result = await _commands.ExecuteAsync(context, argPos, _services);
+
+                if (!result.IsSuccess)
+                    Console.WriteLine(result.ErrorReason);
+            }
+        }
+    }
+}
